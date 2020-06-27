ea50bf1b763084be3d74ebe484c7cd92f1e4b91a
AmakawaYuki <amakawa.yuki@prevent.co.jp>
2020-06-27 11:26:17 +0900

    fix: schema

--- db/migrate/20200614145532_create_contents.rb
+++ db/migrate/20200614145532_create_contents.rb
@@ -2,6 +2,7 @@ class CreateContents < ActiveRecord::Migration[5.2]
   def change
     create_table :contents do |t|
       t.string :url, null: false
+      t.integer :read, null: false, default: 0
       t.timestamps
     end
   end
